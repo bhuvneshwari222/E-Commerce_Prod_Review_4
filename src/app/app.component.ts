d2e3fd406f3823de40aedcc0cee0f6d7dbfd6a0b
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-06-12 00:33:00 +0530

    t-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'E-Commerce_Prod_Review_4';
+  @ViewChild('ecomProdForm') ecomProdForm !: NgForm;
+  onSubmit(){
+    if(this.ecomProdForm.invalid){
+      return this.ecomProdForm.control.markAllAsTouched()
+    }
+    this.ecomProdForm.resetForm();
+  }
 }
